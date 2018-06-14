a0f72fa89f9a2902c48b06b5a59f8c2f1e7ff880
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 19:36:42 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_messages = []
+  count = 0
+  while count < array.length do
+    badge_messages.push(badge_maker(array[count]))
+    count += 1
+  end
+  badge_messages
+end
+
+def assign_rooms(list)
+  
+end
